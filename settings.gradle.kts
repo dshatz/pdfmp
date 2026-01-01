f0d9c784e9868792f1c783620a61bd758857390e
Daniels Šatcs <dev@dshatz.com>
2026-01-01 23:37:05 +0200

    Enable build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,18 @@ dependencyResolutionManagement {
         google()
     }
 }
+
+plugins {
+    id("com.gradle.develocity") version "4.3"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
+
 includeBuild("build-logic")
 include(":pdfmp")
 include(":pdfmp-compose")
